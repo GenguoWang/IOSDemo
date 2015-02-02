dbc2392f06449bd0cdc43151b86037a6ae1ad7da
mark <mark@markdeMacBook-Pro.local>
2015-02-02 11:51:56 +0800

    Initial Commit

---
+++ IOSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSDemo
+//
+//  Created by mark on 15/2/2.
+//  Copyright (c) 2015年 mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSDemo
+//
+//  Created by mark on 15/2/2.
+//  Copyright (c) 2015年 mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
